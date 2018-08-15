fc80a1e7491cdd12117d0e262ec45616b99c8167
M Fhadil <mfhadil05@gmail.com>
2018-08-15 08:18:05 +0700

    TapeEquilibrium

--- lesson/TapeEquilibrium.go
+++ lesson/TapeEquilibrium.go
@@ -4,21 +4,20 @@ import "math"
 
 func Solution(A []int) int {
 	n := len(A)
-	sum := float64(0)
-	for i := 0; i < n; i++ {
-		sum = sum + float64(A[i])
+	sumRight := float64(0)
+	for i := 1; i < n; i++ {
+		sumRight = sumRight + float64(A[i])
 	}
 
-	add := float64(A[0])
-	min := math.Abs((sum - add) - add)
-	if n == 2 {
-		return int(min)
-	}
+	sumLeft := float64(A[0])
+	min := math.Abs(sumRight - sumLeft)
+
 	for i := 1; i < n; i++ {
-		add = add + float64(A[i])
-		if math.Abs((sum-add)-add) < min {
-			min = math.Abs((sum - add) - add)
+		if math.Abs(sumRight-sumLeft) < min {
+			min = math.Abs(sumRight - sumLeft)
 		}
+		sumLeft = sumLeft + float64(A[i])
+		sumRight = sumRight - float64(A[i])
 	}
 	return int(min)
 }
