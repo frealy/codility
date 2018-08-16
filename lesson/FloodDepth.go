0a45816a520a5b54a8f12cb5b2b9f1c41077d1bd
M Fhadil <mfhadil05@gmail.com>
2018-08-16 22:24:15 +0700

    FloodDepth

---
+++ lesson/FloodDepth.go
@@ -0,0 +1,30 @@
+// https://app.codility.com/demo/results/training3NGBSY-JWJ/
+package solution
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func Solution(A []int) int {
+	highestIdx := 0
+	lowestIdx := 0
+	max := 0
+
+	for i := 1; i < len(A); i++ {
+		current := A[i]
+		highest := A[highestIdx]
+		lowest := A[lowestIdx]
+		if current > highest {
+			max = Max(highest-lowest, max)
+			highestIdx = i
+			lowestIdx = highestIdx
+		} else if current > lowest {
+			max = Max(current-lowest, max)
+		} else if current < lowest {
+			lowestIdx = i
+		}
+	}
+	return max
+}
